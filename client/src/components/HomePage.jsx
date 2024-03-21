1b94fd06a3252f75b9bde61d2b501ddb8ecefb6a
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-03-21 15:33:33 -0400

    Set up homepage

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,15 +1,17 @@
 
-function HomePage(){
-    return(<>
-        <div className="full_content">
-            <div className="sidebar">
+function HomePage() {
+    return (<>
+        <div className="full_content h-[100vh] w-[100vw] bg-stone-200 flex items-center">
+            <div className="sidebar h-[97%] min-w-[400px] bg-red-200 rounded-lg shadow-md ml-[10px]">
                 <div className="profile_pic_div"></div>
-                <div className="user_info"></div>                
+                <div className="user_info"></div>
             </div>
-            <nav className='navbar'>
-                
-            </nav>
-            <div className="main_content"></div>
+            <div className="h-[97%] w-[72%] flex flex-col ml-[10px] mr-[10px]">
+                <nav className='navbar h-[100px]  bg-yellow-200 rounded-lg'>
+
+                </nav>
+                <div className="main_content h-[88.5%]  bg-green-100 rounded-lg mt-[10px]"></div>
+            </div>
         </div>
     </>);
 }
